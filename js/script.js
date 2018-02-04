821272bb4905dd1c7b73ccd61ccceac0be3e59af
Jacek Kotlarski <kotlarski.j@gmail.com>
2018-02-04 15:29:15 +0100

    Add project

---
+++ js/script.js
@@ -0,0 +1,15 @@
+// alert ('Hello Yuser in my domain')
+/*var name = prompt('Enter your name');
+alert('Hello, ' + name);
+console.log('Hello, ' + name);*/
+//alert(foo);
+//foo = 'lorem' + ['1', 2, 3.4];     // 'lorem1, 2, 3.4'
+
+var a = 1,
+	h = 1,
+	triangleArea;
+
+a = prompt ('podaj długość boku trójkąta');
+h = prompt ('podaj wysokość trójkąta');
+triangleArea = a*h/2;
+console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
