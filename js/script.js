f071224dbb35d8c299c30d9196989e70714bf9af
Jacek Kotlarski <kotlarski.j@gmail.com>
2018-02-05 21:04:47 +0100

    Add wrong js

--- js/script.js
+++ js/script.js
@@ -1,10 +1,3 @@
-// alert ('Hello Yuser in my domain')
-/*var name = prompt('Enter your name');
-alert('Hello, ' + name);
-console.log('Hello, ' + name);*/
-//alert(foo);
-//foo = 'lorem' + ['1', 2, 3.4];     // 'lorem1, 2, 3.4'
-
 var a = 1,
 	h = 1,
 	triangleArea;
@@ -12,4 +5,4 @@ var a = 1,
 a = prompt ('podaj długość boku trójkąta');
 h = prompt ('podaj wysokość trójkąta');
 triangleArea = a*h/2;
-console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
+console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' : ' + triangleArea);
